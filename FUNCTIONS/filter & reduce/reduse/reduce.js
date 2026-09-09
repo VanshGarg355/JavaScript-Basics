3bbaa43cb3659192abb10d30f4ee81e0d7eaeaeb
VanshGarg355 <vanshgarg355@gmail.com>
2026-09-09 10:12:16 +0530

    okk

--- FUNCTIONS/filter & reduce/reduse/reduce.js
+++ FUNCTIONS/filter & reduce/reduse/reduce.js
@@ -1,24 +1,37 @@
 // .reduce() = reduce the elements of an array to a single value
 
-const prices = [5,30, 10, 25, 15,20];
+const prices = [5, 30, 10, 25, 15, 20];
 
 const total = prices.reduce(sum);
 
 console.log(`$${total.toFixed(2)} `);
 
-function sum(accumulator, element){
-return accumulator + element;
+function sum(accumulator, element) {
+  return accumulator + element;
 }
 
-
-
-
-const grades = [75,50,90,80,65,95];
+const grades = [75, 50, 90, 80, 65, 95];
 
 const maximum = grades.reduce(getMax);
 
 console.log(maximum);
 
-function getMax(accumulator, element){
-return Math.max(accumulator, element);
+function getMax(accumulator, element) {
+  return Math.max(accumulator, element);
 }
+
+let numbers = [10, 20, 30, 40, 50];
+
+let total2 = numbers.reduce((sum, number) => {
+  return sum + number;
+}, 0);
+
+console.log(total2);
+
+let numbers2 = [10, 20, 30, 40, 50];
+
+let total3 = numbers2.reduce((max, number2) => {
+  return number2 > max ? number2 : max;
+}, number2[0]);
+
+console.log(total3);
