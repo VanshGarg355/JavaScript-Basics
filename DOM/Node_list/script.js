0f2ed7b517d3f78f39049a4976ddd8058382e99b
VanshGarg355 <vanshgarg355@gmail.com>
2026-09-25 20:53:53 +0530

    done

---
+++ DOM/Node_list/script.js
@@ -0,0 +1,40 @@
+// NodeList = Static collection of HTML elements by (id, class, element) Can be created by using querySelectorAll() Similar to an array, but no (map, filter, reduce) NodeList won't update to automatically reflect changes
+
+let buttons = document.querySelectorAll(".myButtons");
+
+// MOUSEOVER + MOUSEOUT event listener
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "hsl(205, 100%, 40%)";
+  });
+});
+buttons.forEach((button) => {
+  button.addEventListener(
+    "mouseout",
+    (event) => (event.target.style.backgroundColor = "hsl(205, 100%,60%)"),
+  );
+});
+
+const newButton = document.createElement("button"); //STEP 1
+newButton.textContent = "Button 5"; //STEP 2
+newButton.classList = "myButtons";
+document.body.appendChild(newButton); //STEP 3
+
+console.log(buttons);
+
+// if we want to add in the dom
+
+buttons = document.querySelectorAll(".myButtons");
+
+console.log(buttons);
+
+// // REMOVE AN ELEMENT
+
+// buttons. forEach(button => {
+// button.addEventListener("click", event => {
+// event.target.remove();
+// buttons = document.querySelectorAll(".myButtons");
+// console.log(buttons);
+// });
+// });
